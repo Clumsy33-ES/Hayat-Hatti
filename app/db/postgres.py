e149c2c347210126081577d117caa6a5151c9c69
Clumsy33-ES <alperenelif3@gmail.com>
2025-12-10 16:59:11 +0300

    Add safe postgres async connection config

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -2,16 +2,20 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-
-
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres.osptrzeugafjehxuvqrf:hayathatti2025@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres?sslmode=require")
+# Environment variable yoksa fallback olarak Supabase connection string
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg://postgres.osptrzeugafjehxuvqrf:hayathatti2025"
+    "@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres?sslmode=require"
+)
 
 engine = create_async_engine(
     DATABASE_URL,
     future=True,
     echo=False,
-    pool_pre_ping=True,
+    pool_pre_ping=True,   # bağlantıyı koparsa otomatik düzeltir
+    pool_size=10,         # Railway için ideal
+    max_overflow=20,      # yoğunluk olursa ek bağlantı açar
 )
 
 AsyncSessionLocal = async_sessionmaker(
